f41fe3b5f873c48922892bf8e3c703d66aab898d
Alex <28991173+Cheezfri@users.noreply.github.com>
2023-09-06 14:59:16 -0400

    Added bottomnavigation functionality

--- app/src/main/java/com/example/funfactoftheday/MainActivity.kt
+++ app/src/main/java/com/example/funfactoftheday/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.funfactoftheday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.funfactoftheday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val homePageFragment = HomePageFragment()
+        val categoryFragment = CategoryFragment()
+        val favoriteFactsFragment = FavoriteFactsFragment()
+
+        setCurrentFragment(homePageFragment)
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.miHome ->setCurrentFragment(homePageFragment)
+                R.id.miFavorites -> setCurrentFragment(favoriteFactsFragment)
+                R.id.miCategories -> setCurrentFragment(categoryFragment)
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainer, fragment)
+            commit()
+        }
+
 }
